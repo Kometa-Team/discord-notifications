1d8bc0ea98bb6e3307f0095b023e42652788ff1e
meisnate12 <meisnate12@gmail.com>
2022-03-18 15:59:42 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# discord-notifications
+*Action that uses a webhook for sending automatic Discord messages on new repository releases.*
+
+![alt](https://i.imgur.com/jwo6XsD.png)
+
+### Example usage
+
+Create file `discord-release-msg.yml` under folder `.github/workflows` in your project's root folder:
+>.github/workflows/discord-release-msg.yml
+
+```yml
+
+name: Release messages to discord announcement channel
+
+on:
+  release:
+    types: [ published ]
+
+jobs:
+  run_main:
+    runs-on: ubuntu-18.04
+    name: Sends custom message
+    steps:
+      - name: Sending message
+        uses: meisnate12/discord-release-action@v1.0.0
+        with:
+          webhook_id: ${{ secrets.DISCORD_WEBHOOK_ID }}
+          webhook_token: ${{ secrets.DISCORD_WEBHOOK_TOKEN }}
+```
+
+### Create a webhook
+
+To create a new webhook in discord go to **Channel > Edit > Integrations > Webhooks > New Webhook**
+
+Once you have the webhoook URL, you can get the `ID` and the `TOKEN`. They are included in the URL like so:
+
+> https://discord.com/api/webhooks/ID/TOKEN
+
+
+### Adding necessary secrets
+
+This is needed to not expose your `ID/TOKEN` (which would mean anybody can use your webhook)
+
+In your github project go to **Settings > Secrets > New repository secret**
+
+And add these two secrets (`New repository secret` button):
+
+
+>Name: DISCORD_WEBHOOK_ID
+Value: ID (from url)
+
+>Name: DISCORD_WEBHOOK_TOKEN
+Value: TOKEN (from url)
+
+---
+
+Now every new project release will send a POST using the created webhook, posting a new styled message in the channel where the webhook was created.
+
+
+
---
+++ index.js
@@ -0,0 +1,53 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const fetch = require('node-fetch')
+
+async function run () {
+    const webhookId = core.getInput('webhook_id')
+    const webhookToken = core.getInput('webhook_token')
+
+    if (!webhookId || !webhookToken)
+      return core.setFailed('webhook_id or webhook_token is missing')
+
+    const embedMsg = { }
+    const title = core.getInput('title')
+    embedMsg['color'] = core.getInput('color')
+    const description = core.getInput('description')
+    const url = core.getInput('url')
+    const isRelease = core.getInput('release')
+    if (isRelease) {
+        embedMsg['title'] = title.replace('VERSION', github.context.payload.release.tag_name)
+        release = github.context.payload.release
+        embedMsg['description'] = description.length < 1500 ? release.body : release.body.substring(0, 1500) + ` ([...](${release.html_url}))`
+        embedMsg['url'] = url ? url : release.html_url
+    }
+    else {
+        embedMsg['title'] = title
+        if (description)
+            embedMsg['description'] = description
+        if (url)
+            embedMsg['url'] = url
+    }
+
+    const body = { embeds: [embedMsg] }
+
+    const username = core.getInput('username')
+    if (username)
+        body['username'] = username
+    const avatar_url = core.getInput('avatar_url')
+    if (avatar_url)
+        body['avatar_url'] = avatar_url
+
+    const discord = `https://discord.com/api/webhooks/${core.getInput('webhook_id')}/${core.getInput('webhook_token')}?wait=true`
+
+    fetch(discord, {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => res.json())
+      .then(data => core.info(JSON.stringify(data)))
+      .catch(err => core.info(err))
+}
+
+run()
